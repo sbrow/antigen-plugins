f3020a2138c6d3dce4700b67fa1b0f4d28c4cfd2
Spencer Brower <brower.spencer@gmail.com>
2020-01-31 23:10:38 -0500

    fix(completers): Fixed typo.

--- completers/default.zsh
+++ completers/default.zsh
@@ -1,2 +1,2 @@
-$completers=(_list _oldlist _menu _expand _history _complete _match _ignored _correct _approximate _prefix)
+completers=(_list _oldlist _menu _expand _history _complete _match _ignored _correct _approximate _prefix)
 zstyle ':completion:*' completer $completers
