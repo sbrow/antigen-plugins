cecf8c266121a296fc66d2ade6a1af5a1f94d44c
Spencer Brower <brower.spencer@gmail.com>
2020-02-01 21:23:53 -0500

    fix: Removed completers from global.

--- global.zsh
+++ global.zsh
@@ -1,4 +1,3 @@
-source "${0:A:h}/completers/default.zsh"
 source "${0:A:h}/screen/aliases.zsh"
 source "${0:A:h}/vi-mode-indicator/vi-mode-indicator.plugin.zsh"
 source "${0:A:h}/vi-normal-mode/vi-normal-mode.plugin.zsh"
